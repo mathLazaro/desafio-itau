4a2392631ca95b853ac74bbcf8cc7e2426a2077b
Matheus Lázaro de Lima <matheus.lazaro2002@gmail.com>
2026-01-31 02:00:51 -0300

    init

---
+++ src/main/java/github/mathlazaro/desafioitau/DesafioItauApplication.java
@@ -0,0 +1,13 @@
+package github.mathlazaro.desafioitau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioItauApplication.class, args);
+	}
+
+}
